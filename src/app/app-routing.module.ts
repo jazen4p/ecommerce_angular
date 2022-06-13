9d29e6d5426374ddece7237ef6d5f3d74aead840
jazen4p <46961710+jazen4p@users.noreply.github.com>
2022-06-13 12:23:32 +0700

    Update Routing & Carousel on Home (v2) update with routing and carousel on homepage in ecommerce

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 // import { ModuleWithProviders } from '@angular/core';
 
-import { AboutComponent } from './about/about.component';
-import { ProductsComponent } from './products/products.component';
-import { HomeComponent } from './home/home.component';
-import { CartComponent } from './cart/cart.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { AboutComponent } from "./about/about.component";
+import { ProductsComponent } from "./products/products.component";
+import { HomeComponent } from "./home/home.component";
+import { CartComponent } from "./cart/cart.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'about', component: AboutComponent}
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "about", component: AboutComponent },
+  { path: "products", component: ProductsComponent },
+  { path: "home", component: HomeComponent },
+  { path: "cart", component: CartComponent },
+  { path: "sidebar-component", component: SidebarComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+declare var jQuery:any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    jQuery('.carousel').carousel({
+      interval:2000
+    })
+  }
+  viewProducts() {
+    this.router.navigate(['/products']);
   }
 
 }
